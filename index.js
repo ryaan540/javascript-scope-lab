54bf548c3c9a5f26253155bf5b8540a0f14ae9d2
ryaan540! <rkinyua841@gmail.com>
2025-12-11 23:09:06 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+
+
+// Step 1: Global Variables
+const burgers = ["Hamburger","Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+// Step 2: Function (Function Scope)
+function addBurger(){
+    const newBurger = "Flatburger";   
+    burgers.push(newBurger);        
+    if(true){
+        const anotherNewBurger = "Maple Bacon Burger"; 
+        burgers.push(anotherNewBurger);
+    }
+}
+// Step 3: Block Scope
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake"; 
+};
+
